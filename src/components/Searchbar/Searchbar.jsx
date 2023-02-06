d7dda264395a7c0ec7a86c669e0a1ac5818dea11
Nieves <nievescg98@gmail.com>
2023-02-06 18:26:56 +0100

    Add enter key down & change style

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,15 +1,37 @@
 import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
 import './Searchbar.css'
 
-export default function Searchbar() {
-  const [input, setInput] = useState()
-  const navigateTo = useNavigate()
+export default function Searchbar({ onSearch } ) {
+  const [search, setSearch] = useState('')
+
+  function onClick() {
+    onSearch(search)
+  }
+
+  function onChange(e) {
+    setSearch(e.target.value)
+    if (e.target.value.length === 0) {
+      onSearch(null)
+    }
+  }
+
+  function handleKeyDown(e) {
+    if (e.key === 'Enter') {
+      onClick()
+    }
+  }
 
   return (
-    <div className='search-bar'>
-      <input className='input-search' value={input}/>
-      <button onClick={() => navigateTo()}>Search</button>
+    <div className="search-bar">
+      <input
+        className="input-search"
+        onChange={onChange}
+        onKeyDown={handleKeyDown}
+
+      />
+      <button onClick={onClick}>
+        Search
+      </button>
     </div>
   )
 }
