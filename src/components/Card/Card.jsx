a43d6489d2f92155cdd1c7207b9c0841d2e32a91
Nieves <nievescg98@gmail.com>
2023-02-06 22:33:44 +0100

    Add loader

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { getPokemon } from '../../services/getPokemon'
 import { useNavigate } from 'react-router-dom'
 import { capitalizeFirstLetter } from '../../services/capitalizeFirstLetter'
+import loader from '../../assets/images/loader.svg'
 import './Card.css'
 
 export default function Card({ url }) {
@@ -11,25 +12,27 @@ export default function Card({ url }) {
     setData(res)
   })
   return (
-    <div className="card-wrapper" onClick={() => navigateTo(`pokemon/${data.forms[0].name}`)}>
+    <div className="card-wrapper"
+         onClick={() => navigateTo(`pokemon/${data.forms[0].name}`)}>
       {
         data !== undefined
-          ? (<article className="card-pokemon">
-            <p className="pokemon-id">{`Nº ${data.id}`}</p>
-            <p className="pokemon-name">
-              {capitalizeFirstLetter(data.forms[0].name)}
-            </p>
-            <img
-              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`}
-              alt={`${data.name}`}/>
-            <div className="card-types">
-              {data.types.map(type => (
-                <span key={type.type.name} className={type.type.name}>
+          ? (
+            <article className="card-pokemon">
+              <p className="pokemon-id">{`Nº ${data.id}`}</p>
+              <p className="pokemon-name">
+                {capitalizeFirstLetter(data.forms[0].name)}
+              </p>
+              <img
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`}
+                alt={`${data.name}`}/>
+              <div className="card-types">
+                {data.types.map(type => (
+                  <span key={type.type.name} className={type.type.name}>
 							{type.type.name}
 						</span>))}
-            </div>
-          </article>)
-          : null
+              </div>
+            </article>)
+          : <img src={loader} alt='Loader'/>
       }
     </div>
   )
