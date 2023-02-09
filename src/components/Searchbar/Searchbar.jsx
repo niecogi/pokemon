e1f604cec97c238ef7ef937ad9155474b7edfb00
Nieves <nievescg98@gmail.com>
2023-02-09 14:09:33 +0100

    Change HTML tag

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -6,11 +6,11 @@ export default function Searchbar({ onSearch } ) {
   const [search, setSearch] = useState('')
   const navigateTo = useNavigate()
 
-  function onClick() {
-    onSearch(search, navigateTo)
+  function handleSubmit() {
+    onSearch(search.toLowerCase(), navigateTo)
   }
 
-  function onChange(e) {
+  function handleChange(e) {
     setSearch(e.target.value)
     if (e.target.value.length === 0) {
       onSearch(null)
@@ -19,20 +19,22 @@ export default function Searchbar({ onSearch } ) {
 
   function handleKeyDown(e) {
     if (e.key === 'Enter') {
-      onClick()
+      handleSubmit()
     }
   }
 
   return (
-    <div className="search-bar">
+    <form className="search-bar" onSubmit={handleSubmit}>
       <input
+        type='text'
+        name='query'
+        placeholder='charmander'
         className="input-search"
-        onChange={onChange}
-        onKeyDown={handleKeyDown}
-      />
-      <button onClick={onClick}>
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}/>
+      <button type='submit'>
         Search
       </button>
-    </div>
+    </form>
   )
 }
