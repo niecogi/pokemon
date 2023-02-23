d2a73505bc614cceaadf847cae67902b76c91d96
Nieves <nievescg98@gmail.com>
2023-02-23 14:33:12 +0100

    Add icons to detail pages

--- src/pages/PokemonDetails/PokemonDetails.js
+++ src/pages/PokemonDetails/PokemonDetails.js
@@ -3,12 +3,24 @@ import { useParams, useNavigate } from 'react-router-dom'
 import { PokemonType, ThemeSwitch } from '../../components'
 import { capitalizeFirstLetter } from '../../services/utils'
 import getPokemonByName from '../../services/api'
+import { RiHeart3Fill, RiShieldFill, RiShieldStarFill, RiSwordFill } from 'react-icons/ri'
+import { IoSpeedometer } from 'react-icons/io5'
+import { GiBroadsword } from 'react-icons/gi'
 import './PokemonDetails.css'
 
 
 export default function PokemonDetails() {
   const { pokemonName } = useParams()
   const [pokemon, setPokemon] = useState()
+
+  const icons = {
+    hp: <RiHeart3Fill/>,
+    attack: <GiBroadsword/>,
+    defense: <RiShieldFill/>,
+    'special-defense': <RiShieldStarFill/>,
+    'special-attack': <RiSwordFill/>,
+    speed: <IoSpeedometer/>
+  }
 
   let navigate = useNavigate()
 
@@ -28,31 +40,31 @@ export default function PokemonDetails() {
       <div className="mode-wrapper">
         <ThemeSwitch/>
       </div>
-      { pokemon !== undefined
-          ?
-          <div className="detail-card">
-            <div className="detail-card-background">
-              <img
-                className="detail-image"
-                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
-                alt={`${pokemon.name}`}/>
-            </div>
-            <div className="detail-pokemon-name">
-              <h2>{capitalizeFirstLetter(pokemon.forms[0].name)}</h2>
-            </div>
-           <PokemonType data={pokemon}/>
-            <div className="pokemon-stats">
-              {pokemon.stats.map(stat => (
-                <ul key={stat.id}>
-                  <li>
-                    <p>{`${capitalizeFirstLetter(stat.stat.name.replace('-', ' '))}: ${stat.base_stat}`} </p>
-                  </li>
-                </ul>
-              ))
-              }
-            </div>
+      {pokemon !== undefined
+        ?
+        <div className="detail-card">
+          <div className="detail-card-background">
+            <img
+              className="detail-image"
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
+              alt={`${pokemon.name}`}/>
           </div>
-          : <p>{`❌ Pokemon with the name: ${pokemonName} not found ❌`}</p>
+          <div className="detail-pokemon-name">
+            <h2>{capitalizeFirstLetter(pokemon.forms[0].name)}</h2>
+          </div>
+          <PokemonType data={pokemon}/>
+          <div className="pokemon-stats">
+            {pokemon.stats.map(stat => (
+              <ul key={stat.id}>
+                <li>
+                  <p> {icons[stat.stat.name]} {` ${stat.base_stat} ${capitalizeFirstLetter(stat.stat.name.replace('-', ' '))}`} </p>
+                </li>
+              </ul>
+            ))
+            }
+          </div>
+        </div>
+        : <p>{`❌ Pokemon with the name: ${pokemonName} not found ❌`}</p>
       }
     </div>
   )
